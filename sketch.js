fda81d156fdad84074305eab8fe2865488d8dff8
Federico <federicoilreang@gmail.com>
2021-04-27 16:25:10 +0200

    gif giphy

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+var api = 'https://api.giphy.com/v1/gifs/search?';
+var apiKey = '&api_key=R0JQHUXuv0Cmrshma64Ao9kilP5uLjrf';
+
+var clap = '&q=clap';
+var wow = '&q=wow';
+var dicaprio = '&q=dicaprio';
+
+  function setup() {
+    noCanvas();
+    let risultati = [clap, wow, dicaprio];
+    var query = 'risultato' ;
+    let risultato = random(risultati);
+    var url = api + apiKey + risultato;
+    loadJSON(url, gif);
+  }
+
+  function gif(giphy) {
+    let numbers = ['1','2','3','4','5'];
+    let number = random(numbers);
+    createImg(giphy.data[number].images.original.url);
+  }
