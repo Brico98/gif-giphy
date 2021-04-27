6b45ed9d37f5381afb4fc8a3a8820077665515de
Federico <federicoilreang@gmail.com>
2021-04-27 17:41:26 +0200

    revisione prof, reset button funzionante

--- sketch.js
+++ sketch.js
@@ -2,30 +2,30 @@
 var api = 'https://api.giphy.com/v1/gifs/search?';
 var apiKey = '&api_key=R0JQHUXuv0Cmrshma64Ao9kilP5uLjrf';
 
-var clap = '&q=clap';
+var clap = '&q=dick';
 var wow = '&q=wow';
 var dicaprio = '&q=dicaprio';
 
-var queryValue;
+var img;
 
-function setup() {
-  noCanvas();
-  slider = createSlider(0,100,20);
-  var url = api + apiKey + queryValue;
-  loadJSON(url, gif);
-}
-
-function gif(giphy) {
-  createImg(giphy.data[0].images.original.url);
-}
-
-function draw() {
-  var valoreSlider = slider.value();
-
-
-  if (valoreSlider < 33) {
-    queryValue = dicaprio;
-    gif();
+  function setup() {
+    noCanvas();
+    var button = createButton('reset');
+    button.mousePressed(resetSketch);
+    resetSketch();
   }
 
+function resetSketch(){
+  let risultati = [clap, wow, dicaprio];
+  var query = 'risultato' ;
+  let risultato = random(risultati);
+  var url = api + apiKey + risultato;
+  loadJSON(url, gif);
+  img.remove();
 }
+
+  function gif(giphy) {
+    let numbers = ['1','2','3','4','5'];
+    let number = random(numbers);
+    img = createImg(giphy.data[number].images.original.url);
+  }
