bdfd641013f339deccc35343f5a59c6abd01a24b
Federico <federicoilreang@gmail.com>
2021-04-27 17:28:31 +0200

    Merge branch 'master' of https://github.com/Brico98/gif-giphy

--- sketch.js
+++ sketch.js
@@ -2,21 +2,30 @@
 var api = 'https://api.giphy.com/v1/gifs/search?';
 var apiKey = '&api_key=R0JQHUXuv0Cmrshma64Ao9kilP5uLjrf';
 
-var clap = '&q=dick';
-var wow = '&q=vagina';
-var dicaprio = '&q=fuck';
+var clap = '&q=clap';
+var wow = '&q=wow';
+var dicaprio = '&q=dicaprio';
 
-  function setup() {
-    noCanvas();
-    let risultati = [clap, wow, dicaprio];
-    var query = 'risultato' ;
-    let risultato = random(risultati);
-    var url = api + apiKey + risultato;
-    loadJSON(url, gif);
+var queryValue;
+
+function setup() {
+  noCanvas();
+  slider = createSlider(0,100,20);
+  var url = api + apiKey + queryValue;
+  loadJSON(url, gif);
+}
+
+function gif(giphy) {
+  createImg(giphy.data[0].images.original.url);
+}
+
+function draw() {
+  var valoreSlider = slider.value();
+
+
+  if (valoreSlider < 33) {
+    queryValue = dicaprio;
+    gif();
   }
 
-  function gif(giphy) {
-    let numbers = ['1','2','3','4','5'];
-    let number = random(numbers);
-    createImg(giphy.data[number].images.original.url);
-  }
+}
