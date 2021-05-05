1bf59771c3687d7844e356ba461d25b325f5cd1f
lucasabatini99 <60349629+lucasabatini99@users.noreply.github.com>
2021-05-05 11:47:46 +0200

    reorder funzionante

--- sketch.js
+++ sketch.js
@@ -48,6 +48,9 @@ function timestamp(dataImport){
       console.log(item, index);
     })
 
-    var minimo = Math.min(oldNew.length); // 1
+
+
+
+    var minimo = oldNew.sort(function(a, b){return a-b});  // 1
     console.log(minimo);
 }
